9b15e648b0edc627ea6268c2e87cd6d767f06bc4
rfist <theseuship@gmail.com>
2018-03-09 23:27:12 +0200

    few fixes for project settings

--- utils/generate_puzzle.js
+++ utils/generate_puzzle.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const [,, year, day, puzzlesAmount = 2] = process.argv;
 const path = __dirname + "\\..\\src\\puzzles\\";
-let puzzleNumber = 0;
 
 for (let puzzleNumber = 0; puzzleNumber < puzzlesAmount; puzzleNumber++) {
     const codeTemplate =
@@ -19,12 +18,13 @@ test('Year ${year}, Day ${day}, puzzle ${puzzleNumber}', () => {
     expect(exec(1)).toBe(1);
 });
     `;
-    fs.writeFile(`${path}puzzle_${year}_${day}_${puzzleNumber}.js`, codeTemplate, (err) => {
-        if (err) throw err;
-        console.log(`File ${path}puzzle_${year}_${day}_${puzzleNumber}.js has been generated!`);
-    });
-    fs.writeFile(`${path}puzzle_${year}_${day}_${puzzleNumber}.test.js`, testTemplate, (err) => {
-        if (err) throw err;
-        console.log(`File ${path}puzzle_${year}_${day}_${puzzleNumber}.test.js has been generated!`);
-    });
+
+fs.writeFile(`${path}puzzle_${year}_${day}_${puzzleNumber}.js`, codeTemplate, (err) => {
+    if (err) throw err;
+    console.log(`File ${path}puzzle_${year}_${day}_${puzzleNumber}.js has been generated!`);
+});
+fs.writeFile(`${path}puzzle_${year}_${day}_${puzzleNumber}.test.js`, testTemplate, (err) => {
+    if (err) throw err;
+    console.log(`File ${path}puzzle_${year}_${day}_${puzzleNumber}.test.js has been generated!`);
+});
 }
